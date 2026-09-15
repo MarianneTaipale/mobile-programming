8489bc819fa8fd27b6295d767788c15d5dd074a6
MarianneTaipale <marianne.taipale@gmail.com>
2026-09-15 16:17:31 +0300

    Tehtava 6 tehty

--- Harjoitus_tehtavat/Tehtava_6/App.tsx
+++ Harjoitus_tehtavat/Tehtava_6/App.tsx
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { TextInput } from 'react-native';
+import { Button } from 'react-native';
+import { FlatList } from 'react-native';
+import { Image } from 'react-native';
+
+type Meals = {
+  idMeal: number;
+  strMeal: string;
+  strMealThumb: string;
+}
+
 
 export default function App() {
+  const [ingredient, setIngredient] = useState('');
+  const [meals, setMeals] = useState<Meals[]>([]);
+
+  const handleFetch = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error in fetch:" + response.statusText);
+
+        return response.json()
+      })
+      .then(data => setMeals(data.meals))
+      .catch(err => console.error(err));
+  }
   return (
     <View style={styles.container}>
-      <Text>Tehtava 6</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder='Enter ingredient'
+        value={ingredient}
+        onChangeText={text => setIngredient(text)}
+      />
+      <Button title="FIND" onPress={handleFetch} />
+      <FlatList
+        data={meals}
+        renderItem={({ item }) =>
+          <View>
+            <Text style={styles.title}>
+              {item.strMeal}
+            </Text>
+            <Image
+              source={{ uri: item.strMealThumb }}
+              style={styles.image}
+            />
+          </View>}
+      />
     </View>
   );
 }
@@ -14,7 +58,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 20,
+    paddingTop: 60,
+  },
+
+  input: {
+    fontSize: 18,
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+
+  recipe: {
+    marginTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    width: 250,
+    height: 250,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+    marginBottom: 10,
   },
 });
